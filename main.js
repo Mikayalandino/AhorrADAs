e9d11c33c23ee8d45891bdcf1f1f725ab085adb5
Mika <Micaelayalandino@gmail.com>
2021-11-04 17:39:36 -0300

    se agregó la funcionalidad del botón de categorías

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const seccionBalance = document.getElementById("seccion-balance");
 const seccionNuevaOperacion = document.getElementById("seccion-nueva-operacion");
+const seccionCategorias = document.getElementById("seccion-categorias")
 
 const botonNuevaOperacion = document.getElementById("boton-operacion");
 const botonCancelarOperacion = document.getElementById("boton-cancelar-operacion");
 const botonCambiarFiltros = document.getElementById("boton-cambiar-filtros");
 const contenedorFiltros = document.getElementById("cambiar-filtros"); 
+const botonCategorias = document.getElementById("boton-categorias")
+const botonAgregarNombreCategoria = document.getElementById("boton-agregar-categoria")
 
 const formularioFiltros = document.getElementById("formulario-filtros");
 
@@ -29,3 +32,15 @@ botonCambiarFiltros.onclick = (event) => {
       botonCambiarFiltros.textContent = "Ocultar filtros";
     }
   };
+
+// seccion categorías
+
+                                // botón categorías
+
+botonCategorias.onclick = () => {
+  seccionCategorias.classList.remove("is-hidden");
+  seccionBalance.classList.add("is-hidden")
+}
+
+
+
