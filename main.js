3965592ab6e102afba498c4701fd56f1dea6dca9
Mika <Micaelayalandino@gmail.com>
2021-11-05 17:30:17 -0300

    se agregó la lista de categorías y se probó funcionalidades en JS

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const seccionBalance = document.getElementById("seccion-balance");
 const seccionNuevaOperacion = document.getElementById("seccion-nueva-operacion");
-const seccionCategorias = document.getElementById("seccion-categorias")
-
+//categorías
+const seccionCategorias = document.getElementById("seccion-categorias");
+const listadoDeCategorias = document.getElementById("listado-categorias");
+const categoriasAgregadas = document.getElementById("categorias-agregadas");
+const selectCategorias = document.getElementById("select-categorias")
+//
 const botonNuevaOperacion = document.getElementById("boton-operacion");
 const botonCancelarOperacion = document.getElementById("boton-cancelar-operacion");
 const botonCambiarFiltros = document.getElementById("boton-cambiar-filtros");
 const contenedorFiltros = document.getElementById("cambiar-filtros"); 
 
-const botonBalance = document.getElementById("boton-balance")
-
-const botonCategorias = document.getElementById("boton-categorias")
-const botonAgregarNombreCategoria = document.getElementById("boton-agregar-categoria")
-
+const botonBalance = document.getElementById("boton-balance");
+// botones categorías
+const botonNavCategorias = document.getElementById("boton-nav-categorias");
+const botonAgregarCategoria = document.getElementById("boton-agregar-categoria");
+//
 const formularioFiltros = document.getElementById("formulario-filtros");
+
 
 
 const modificarClasesBotones = (boton, clase1, clase2) => {
@@ -50,6 +55,52 @@ botonCategorias.onclick = () => {
   seccionCategorias.classList.remove("is-hidden");
   seccionBalance.classList.add("is-hidden")
 }
+// Lista de categorías
 
+// const categorias = ["Comida", "Servicios", "Salidas", "Educación", "Transporte", "Trabajo"]
 
+// const obtenerCategorias = () => {
+//   const categoriasEnLocalStorage = localStorage.getItem("categorias")
+//   if (categoriasEnLocalStorage === null) {
+//     return categorias
+//   } 
+//   else {
+//     return JSON.parse(categoriasEnLocalStorage)
+//   }
+// }
 
+// const agregarCategoriasAlSelect = () => {
+//   const categorias = obtenerCategorias()
+//   const select = document.querySelector("#categoria-select")
+
+//   const categoriasString = categorias.reduce((acc, elemento) => {
+//     return acc + `<option value="${elemento}">${elemento}</option>`
+//   }, "")
+  
+//   select.innerHTML = categoriasString
+// }
+
+// const agregarCategoriasAHTML = () => {
+//   const categorias = obtenerCategorias()
+
+//   const categoriasString = categorias.reduce((acc, elemento, index) => {
+//     return acc + `<div>${elemento} <button id="eliminar-${index}" class="boton-eliminar delete-link">Eliminar</button></div>`
+//   }, "")
+  
+//   listadoDeCategorias.innerHTML = categoriasString
+// }
+
+// agregarCategoriasAHTML()
+
+// botonAgregarCategoria.onclick = () => {
+//   const nuevaCategoria = listadoDeCategorias.value
+//   const categorias = obtenerCategorias()
+//   categorias.push(nuevaCategoria)
+//   listadoDeCategorias.value = ""
+
+//   const categoriasAJSON = JSON.stringify(categorias)
+//   localStorage.setItem("categorias", categoriasAJSON)
+
+//   agregarCategoriasAlSelect()
+//   agregarCategoriasAHTML()
+// }
