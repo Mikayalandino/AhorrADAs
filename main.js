ff894713f7a79e82de8fb8b71e259a79f8ea918e
Elizabeth Carolina <celerner4@gmail.com>
2021-11-07 18:22:53 -0300

    idento html y js

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ const botonConfirmarEditarCategoria = document.getElementById("boton-confirmar-e
 
 // FUNCIONES GENÉRICAS REUTILIZABLES 
 const modificarClasesBotones = (boton, clase1, clase2) => {
-  boton.onclick = () => {
-    clase1.classList.add("is-hidden")
-    clase2.classList.remove("is-hidden")
-  }
+boton.onclick = () => {
+  clase1.classList.add("is-hidden")
+  clase2.classList.remove("is-hidden")
+}
 }
 
 const aJSONYSubirAlLStorage = (operacion, clave) => {
@@ -38,9 +38,9 @@ localStorage.setItem(clave , aJSON)
 }
 
 const obtenerLStorageYPasarAJs = (clave) => {
-    const nuevoObjeto = localStorage.getItem(clave) || `[]`
-    const JSONAObjeto = JSON.parse(nuevoObjeto)     
-    return JSONAObjeto
+  const nuevoObjeto = localStorage.getItem(clave) || `[]`
+  const JSONAObjeto = JSON.parse(nuevoObjeto)     
+  return JSONAObjeto
 }
 
 // OPERACIONES
@@ -54,68 +54,68 @@ modificarClasesBotones(botonCancelarOperacion, seccionNuevaOperacion, seccionBal
 
 
 botonCambiarFiltros.onclick = (event) => {
-  event.preventDefault();
-  contenedorFiltros.classList.toggle("is-hidden");
-  if (contenedorFiltros.classList.contains("is-hidden")) {
-    botonCambiarFiltros.textContent = "Mostrar filtros";
-  } else {
-    botonCambiarFiltros.textContent = "Ocultar filtros";
-  }
+event.preventDefault();
+contenedorFiltros.classList.toggle("is-hidden");
+if (contenedorFiltros.classList.contains("is-hidden")) {
+  botonCambiarFiltros.textContent = "Mostrar filtros";
+} else {
+  botonCambiarFiltros.textContent = "Ocultar filtros";
+}
 };
 
 /* // Botón balance
 
 botonBalance.onclick = () => {
-  
+
 } */
 
 // Sección categorías
 
-                                // botón nav categorías
+                              // botón nav categorías
 
 botonNavCategorias.onclick = () => {
-  seccionCategorias.classList.remove("is-hidden");
-  seccionEditarCategorias.classList.add("is-hidden")
-  seccionBalance.classList.add("is-hidden")
-  seccionNuevaOperacion.classList.add("is-hidden")
+seccionCategorias.classList.remove("is-hidden");
+seccionEditarCategorias.classList.add("is-hidden")
+seccionBalance.classList.add("is-hidden")
+seccionNuevaOperacion.classList.add("is-hidden")
 }
 
-                                // CATEGORÍAS 
+                              // CATEGORÍAS 
 
 let categorias = ["Todas", "Comida", "Servicios", "Salidas", "Educación", "Transporte", "Trabajo"]
 
 
 
 const subirCategorias = (boton, arr, clave) => {
-  boton.onclick = () => {
-    arr.push(inputSeccionCategoria.value)
-    aJSONYSubirAlLStorage(arr, clave)    
-  }  
+boton.onclick = () => {
+  arr.push(inputSeccionCategoria.value)
+  aJSONYSubirAlLStorage(arr, clave)    
+}  
 }
 
 subirCategorias(botonInputSeccionCategoria, categorias, "categorias")
 
 categorias = obtenerLStorageYPasarAJs("categorias") 
- 
+
 const arrayReduc = categorias.reduce((acc, arr) => {
-    return acc += `<option value="${arr}">${arr}</option>`
+  return acc += `<option value="${arr}">${arr}</option>`
 }, "")
 
 selectCategoriasDeFiltros.innerHTML = arrayReduc
 
 const agregarCategoriasAHTML = () => {
-  
-  const categoriasHTML = categorias.reduce((acc, elemento) => {
-    return acc + `<div class="columns">
-    <div class="column">
-        <div class="tag is-primary is-light">${elemento}</div>
-    </div>
-    <button id="listaDeBotonesEditarCategoria" class= "button is-ghost is-small mr-2 mt-2">Editar</button> 
-    <button id="listaDeBotonesEliminarCategoria" class= "button is-ghost is-small mr-1 mt-2">Eliminar</button>
-    </div>`
-  }, "")
 
-  listadoDeCategorias.innerHTML = categoriasHTML
+const categoriasHTML = categorias.reduce((acc, elemento) => {
+  return acc + `<div class="columns">
+  <div class="column">
+      <div class="tag is-primary is-light">${elemento}</div>
+  </div>
+  <button id="listaDeBotonesEditarCategoria" class= "button is-ghost is-small mr-2 mt-2">Editar</button> 
+  <button id="listaDeBotonesEliminarCategoria" class= "button is-ghost is-small mr-1 mt-2">Eliminar</button>
+  </div>`
+}, "")
+
+listadoDeCategorias.innerHTML = categoriasHTML
 }
 
 agregarCategoriasAHTML()
@@ -124,46 +124,46 @@ agregarCategoriasAHTML()
 /**
 
 botonAgregarCategoria.onclick = () => {
-  const nuevaCategoria = listadoDeCategorias.value
-  const categorias = obtenerCategorias()
-  categorias.push(nuevaCategoria)
-  listadoDeCategorias.value = ""
+const nuevaCategoria = listadoDeCategorias.value
+const categorias = obtenerCategorias()
+categorias.push(nuevaCategoria)
+listadoDeCategorias.value = ""
 
-  const categoriasAJSON = JSON.stringify(categorias)
-  localStorage.setItem("categorias", categoriasAJSON)
+const categoriasAJSON = JSON.stringify(categorias)
+localStorage.setItem("categorias", categoriasAJSON)
 
-   agregarCategoriasAlSelect() 
-  agregarCategoriasAHTML()
+  agregarCategoriasAlSelect() 
+agregarCategoriasAHTML()
 }
 
-                                // Editar lista categorías
+                              // Editar lista categorías
 
 // Lista de botones editar-eliminar
 
 
 for (let i = 0; i < listaDeBotonesEliminarCategoria.length; i++) {
-  listaDeBotonesEliminarCategoria[i].onclick = () => {
-    const id = listaDeBotonesEliminarCategoria[i].id
-    console.log(id)
-    const indice = id.charAt(7)
-    const categoriasFiltradas = categorias.filter((elemento, index) => {
-        return index != indice
-      })
-  }
+listaDeBotonesEliminarCategoria[i].onclick = () => {
+  const id = listaDeBotonesEliminarCategoria[i].id
+  console.log(id)
+  const indice = id.charAt(7)
+  const categoriasFiltradas = categorias.filter((elemento, index) => {
+      return index != indice
+    })
+}
 }
 
 for (let i = 0; i < listaDeBotonesEditarCategoria.length; i++) {
-  listaDeBotonesEditarCategoria[i].onclick = () => {
-    seccionEditarCategorias.classList.remove("is-hidden");
-    seccionCategorias.classList.add("is-hidden");
-    seccionBalance.classList.add("is-hidden")
-    seccionNuevaOperacion.classList.add("is-hidden")
-  }
+listaDeBotonesEditarCategoria[i].onclick = () => {
+  seccionEditarCategorias.classList.remove("is-hidden");
+  seccionCategorias.classList.add("is-hidden");
+  seccionBalance.classList.add("is-hidden")
+  seccionNuevaOperacion.classList.add("is-hidden")
 }
- 
+}
+
 botonCancelarEditarCategoria.onclick = () => {
-  seccionEditarCategorias.classList.add("is-hidden");
-  seccionCategorias.classList.remove("is-hidden")
+seccionEditarCategorias.classList.add("is-hidden");
+seccionCategorias.classList.remove("is-hidden")
 
 }
 
